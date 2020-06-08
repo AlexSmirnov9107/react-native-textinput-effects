10374b754b9a7874a33df9d7b0c9c9a182e1d86f
Alexandr Smirnov <smirnov.a.n.9107@gmail.com>
2020-06-09 00:44:08 +0600

    types Hoshi

--- types/types.d.ts
+++ types/types.d.ts
@@ -69,6 +69,7 @@ declare module "react-native-textinput-effects" {
   export interface HoshiProps extends CommonProps {
     maskColor?: string;
     borderColor?: string;
+    activeColor?: string;
   }
 
   class Hoshi extends BaseClass<HoshiProps> {}
